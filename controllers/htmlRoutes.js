45bf226186b935e2b62e8d67793741275d05d563
Soloman Garcia <soloman.garcia@gmail.com>
2019-11-14 11:44:56 -0700

    test

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -1,22 +1,22 @@
-// var path = require("path");
+var path = require("path");
 
 
-// module.exports = function(app){
+module.exports = function(app){
 
-// console.log("html routes page")
-// app.get("/", function(req, res){
+console.log("html routes page")
+app.get("/", function(req, res){
 
-// 	console.log("index.html has rendered")
-// 	res.sendFile(path.join(__dirname, '../client/public/index.html'));
+	console.log("index.html has rendered")
+	res.sendFile(path.join(__dirname, '../client/public/index.html'));
+})
+
+// app.get("/admin", function(req, res){
+// 	console.log("html rendering")
+
+// 	res.sendFile(path.join(__dirname, '../client/public/admin.html'));
 // })
 
-// // app.get("/admin", function(req, res){
-// // 	console.log("html rendering")
-
-// // 	res.sendFile(path.join(__dirname, '../client/public/admin.html'));
-// // })
 
 
 
-
-// }
+}
